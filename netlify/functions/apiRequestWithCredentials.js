fa12a6eaa52ac46d258dfef84b80ad0acbaa4d1c
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-11-16 13:39:34 +0100

    add sample function & request

---
+++ netlify/functions/apiRequestWithCredentials.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const handler = async (event, context) => {
+    
+    const cityName = "amsterdam";
+    const API_KEY = process.env.API_KEY;
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
+
+    const response = await axios.get(API_URL);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            info: response.data
+        })
+    }
+}
