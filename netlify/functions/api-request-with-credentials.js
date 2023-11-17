0909cff2cb7432feb571a554d903c5a7cf3b5d0f
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-11-17 14:54:32 +0100

    Revert "use node-fetch instead of axios"
    This reverts commit 17f70035dcabd74a7c2e93463f74a3fc46d6d7e1.

--- netlify/functions/api-request-with-credentials.js
+++ netlify/functions/api-request-with-credentials.js
@@ -1,18 +1,17 @@
-import fetch from 'node-fetch'
+const axios = require("axios");
 
 export const handler = async (event, context) => {
-
+    
     const cityName = "amsterdam";
     const API_KEY = process.env.API_KEY;
     const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
 
-    const response = await fetch(API_URL);
-    const data = await response.json()
+    const response = await axios.get(API_URL);
 
     return {
         statusCode: 200,
         body: JSON.stringify({
-            info: data
+            info: response.data
         })
     }
 }
