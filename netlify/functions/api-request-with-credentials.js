17f70035dcabd74a7c2e93463f74a3fc46d6d7e1
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-11-17 14:49:40 +0100

    use node-fetch instead of axios

--- netlify/functions/api-request-with-credentials.js
+++ netlify/functions/api-request-with-credentials.js
@@ -1,17 +1,18 @@
-const axios = require("axios");
+import fetch from 'node-fetch'
 
 export const handler = async (event, context) => {
-    
+
     const cityName = "amsterdam";
     const API_KEY = process.env.API_KEY;
     const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
 
-    const response = await axios.get(API_URL);
+    const response = await fetch(API_URL);
+    const data = await response.json()
 
     return {
         statusCode: 200,
         body: JSON.stringify({
-            info: response.data
+            info: data
         })
     }
 }
